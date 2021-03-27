a15f743968214cdefaef9f28afc40ed33d62c508
jasperazerbaijan <80984562+jasperazerbaijan@users.noreply.github.com>
2021-03-27 13:43:14 +0400

    Update antiflood.py

--- tg_bot/modules/antiflood.py
+++ tg_bot/modules/antiflood.py
@@ -35,7 +35,7 @@ def check_flood(bot: Bot, update: Update) -> str:
 
     try:
         chat.kick_member(user.id)
-        msg.reply_text("dont disturb others you are No need for this group anymore...")
+        msg.reply_text("Başqalarına ehtiyacınız yoxdur, artıq bu qrupa ehtiyacınız yoxdur...")
 
         return "<b>{}:</b>" \
                "\n#BANNED" \
@@ -44,11 +44,11 @@ def check_flood(bot: Bot, update: Update) -> str:
                                              mention_html(user.id, user.first_name))
 
     except BadRequest:
-        msg.reply_text("You cannot use this service as long as you do not give me Permissions.")
+        msg.reply_text("Mənə İcazələr verməyincə bu xidmətdən istifadə edə bilməzsiniz.")
         sql.set_flood(chat.id, 0)
         return "<b>{}:</b>" \
-               "\n#INFO" \
-               "\nDon't have kick permissions, so automatically disabled antiflood.".format(chat.title)
+               "\n#Məlumat" \
+               "\nİcazələrinizi yoxlayın, buna görə də flood-a avtomatik olaraq aradan qaldırın.".format(chat.title)
 
 
 @run_async
@@ -64,33 +64,33 @@ def set_flood(bot: Bot, update: Update, args: List[str]) -> str:
         val = args[0].lower()
         if val == "off" or val == "no" or val == "0":
             sql.set_flood(chat.id, 0)
-            message.reply_text("I will no longer dismiss those who flood.")
+            message.reply_text("Artıq flood edənləri qrupdan azad etmərəm.")
 
         elif val.isdigit():
             amount = int(val)
             if amount <= 0:
                 sql.set_flood(chat.id, 0)
-                message.reply_text("I will no longer dismiss those who flood.")
+                message.reply_text("Artıq flood edənləri qrupdan azad etmərəm.")
                 return "<b>{}:</b>" \
-                       "\n#SETFLOOD" \
+                       "\n#Flood Ayarları" \
                        "\n<b>Admin:</b> {}" \
-                       "\nDisabled antiflood.".format(html.escape(chat.title), mention_html(user.id, user.first_name))
+                       "\nAntiflood deaktiv.".format(html.escape(chat.title), mention_html(user.id, user.first_name))
 
             elif amount < 3:
-                message.reply_text("Antiflood has to be either 0 (disabled), or a number bigger than 3!")
+                message.reply_text("Antiflood ya 0 (deaktiv), ya da 3-dən böyük bir rəqəm olmalıdır!")
                 return ""
 
             else:
                 sql.set_flood(chat.id, amount)
-                message.reply_text("Message control {} has been added to count ".format(amount))
+                message.reply_text("Mesaj nəzarəti {} saymaq üçün əlavə edildi".format(amount))
                 return "<b>{}:</b>" \
-                       "\n#SETFLOOD" \
+                       "\n#Flood Ayarları" \
                        "\n<b>Admin:</b> {}" \
-                       "\nSet antiflood to <code>{}</code>.".format(html.escape(chat.title),
+                       "\nAntiflood Ayarları <code>{}</code>.".format(html.escape(chat.title),
                                                                     mention_html(user.id, user.first_name), amount)
 
         else:
-            message.reply_text("I don't understand what you're saying .... Either use the number or use Yes-No")
+            message.reply_text("Dediklərinizi anlamıram .... Ya nömrədən istifadə edin, ya da Bəli-Xeyrdən istifadə edin.")
 
     return ""
 
@@ -101,10 +101,10 @@ def flood(bot: Bot, update: Update):
 
     limit = sql.get_flood_limit(chat.id)
     if limit == 0:
-        update.effective_message.reply_text("I am not doing message control right now!")
+        update.effective_message.reply_text("Mən hazırda mesaj nəzarəti etmirəm!")
     else:
         update.effective_message.reply_text(
-            " {} I'll leave the bun to the person who sends the message more at the same time.".format(limit))
+            " {} Bunu eyni zamanda daha çox mesaj göndərən şəxsə buraxacağam.".format(limit))
 
 
 def __migrate__(old_chat_id, new_chat_id):
@@ -114,16 +114,16 @@ def __migrate__(old_chat_id, new_chat_id):
 def __chat_settings__(chat_id, user_id):
     limit = sql.get_flood_limit(chat_id)
     if limit == 0:
-        return "*Not* currently enforcing flood control."
+        return "Hal-hazırda flodd nəzarəti *edilmir*. "
     else:
-        return " The message control is set to `{}`.".format(limit)
+        return " Mesaj nəzarəti ayarlandı `{}`.".format(limit)
 
 
 __help__ = """
- - /flood: To know your current message control..
+ - /flood: Cari mesaj nəzarətinizi bilmək üçün ..
 
 *Admin only:*
- - /setflood <int/'no'/'off'>: enables or disables flood control
+ - /setflood <int/'no'/'off'>: aktiv və ya deaktiv etmək
 """
 
 __mod_name__ = "AntiFlood"
