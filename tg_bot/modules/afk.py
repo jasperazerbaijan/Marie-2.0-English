a39267d3c6f254f92d0806ea82d044448d5e2614
jasperazerbaijan <80984562+jasperazerbaijan@users.noreply.github.com>
2021-03-27 13:24:45 +0400

    Update afk.py

--- tg_bot/modules/afk.py
+++ tg_bot/modules/afk.py
@@ -22,7 +22,7 @@ def afk(bot: Bot, update: Update):
         reason = ""
 
     sql.set_afk(update.effective_user.id, reason)
-    update.effective_message.reply_text("{} is away from the keyboard ! ".format(update.effective_user.first_name))
+    update.effective_message.reply_text("{} klaviaturadan kənarda ! ".format(update.effective_user.first_name))
 
 
 @run_async
@@ -34,7 +34,7 @@ def no_longer_afk(bot: Bot, update: Update):
 
     res = sql.rm_afk(user.id)
     if res:
-        update.effective_message.reply_text("{} Not far from the keyboard now !".format(update.effective_user.first_name))
+        update.effective_message.reply_text("{} indi klaviaturadan uzaqdadır !".format(update.effective_user.first_name))
 
 
 @run_async
@@ -61,17 +61,17 @@ def reply_afk(bot: Bot, update: Update):
             if sql.is_afk(user_id):
                 user = sql.check_afk_status(user_id)
                 if not user.reason:
-                    res = "{} is away from the keyboard ! reason :\n{} ".format(fst_name)
+                    res = "{} klaviaturadan uzaqdır! səbəb :\n{} ".format(fst_name)
                 else:
-                    res = "{} is away from the keyboard ! reason :\n{}. ".format(fst_name, user.reason)
+                    res = "{} klaviaturadan uzaqdır! səbəb :\n{}. ".format(fst_name, user.reason)
                 message.reply_text(res)
 
 
 __help__ = """
- - /afk <reason>: mark yourself as AFK.
- - brb <reason>: same as the afk command - but not a command.
+ - /afk <reason>: özünüzü AFK kimi qeyd edin.
+ - /brb <reason>: afk əmri ilə eyni - ancaq bir əmr deyil.
 
-When marked as AFK, any mentions will be replied to with a message to say you're not available!
+AFK olaraq qeyd edildikdə, hər hansı bir qeyd mövcud olmadığınızı bildirən bir mesajla cavablandırılacaqdır!
 """
 
 __mod_name__ = "AFK"
